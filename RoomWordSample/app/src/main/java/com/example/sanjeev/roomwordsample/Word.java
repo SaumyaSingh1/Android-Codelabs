124d10cedb6dde55546a99b31e2576c608f6162f
Sanjeev <thedevelopersanjeev@gmail.com>
2018-08-03 20:00:06 +0530

    added word entity class

---
+++ RoomWordSample/app/src/main/java/com/example/sanjeev/roomwordsample/Word.java
@@ -0,0 +1,33 @@
+package com.example.sanjeev.roomwordsample;
+
+
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+import android.support.annotation.NonNull;
+
+/**
+ * This class defines a word entity.
+ * words are the data for our app.
+ */
+
+@Entity(tableName = "word_table")
+public class Word {
+
+    // Auto Generate Primary Keys
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    @NonNull
+    @ColumnInfo(name = "word")  // By default, column name is same as member variable
+    private String mWord;
+
+    public Word(@NonNull String word){
+        this.mWord = word;
+    }
+
+    // Each field in database must be either public or have a getter method
+    public String getWord(){
+        return this.mWord;
+    }
+}
