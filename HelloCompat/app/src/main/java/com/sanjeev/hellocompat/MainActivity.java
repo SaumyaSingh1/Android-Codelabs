1025eb8f99e3075ea85b4325b44d5ccfef9ac162
Sanjeev <thedevelopersanjeev@gmail.com>
2018-09-25 23:03:00 +0530

    completed hellocompat codelab

--- HelloCompat/app/src/main/java/com/sanjeev/hellocompat/MainActivity.java
+++ HelloCompat/app/src/main/java/com/sanjeev/hellocompat/MainActivity.java
@@ -1,10 +1,13 @@
 package com.sanjeev.hellocompat;
 
 import android.os.PersistableBundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,7 +30,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void changeColor(View view) {
-
+        Random random = new Random();
+        String colorName = mColorArray[random.nextInt(20)];
+        int colorResourceName = getResources().getIdentifier(colorName, "color", getApplicationContext().getPackageName());
+        int colorRes = ContextCompat.getColor(this, colorResourceName);
+        mHelloTextView.setTextColor(colorRes);
     }
 
     @Override
