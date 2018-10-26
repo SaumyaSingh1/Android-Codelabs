6451d1887a3fe953093c5a4df6c857ffef9b420c
Sanjeev <thedevelopersanjeev@gmail.com>
2018-10-26 20:49:53 +0530

    Task 1 completed in CustomReceiver app

--- PowerReceiver/app/src/main/java/com/developersanjeev/powerreceiver/CustomReceiver.java
+++ PowerReceiver/app/src/main/java/com/developersanjeev/powerreceiver/CustomReceiver.java
@@ -3,12 +3,25 @@ package com.developersanjeev.powerreceiver;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.widget.Toast;
 
 public class CustomReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
 
-        throw new UnsupportedOperationException("Not yet implemented");
+        String intentAction = intent.getAction();
+        if(intentAction != null){
+            String toastMessage = "unknown intent action";
+            switch (intentAction){
+                case Intent.ACTION_POWER_CONNECTED :
+                    toastMessage = "Power Connected";
+                    break;
+                case Intent.ACTION_POWER_DISCONNECTED :
+                    toastMessage = "Power Disconnected";
+                    break;
+            }
+            Toast.makeText(context, toastMessage, Toast.LENGTH_SHORT).show();
+        }
     }
 }
--- PowerReceiver/app/src/main/java/com/developersanjeev/powerreceiver/MainActivity.java
+++ PowerReceiver/app/src/main/java/com/developersanjeev/powerreceiver/MainActivity.java
@@ -1,13 +1,31 @@
 package com.developersanjeev.powerreceiver;
 
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private CustomReceiver mReceiver;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mReceiver = new CustomReceiver();
+
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(Intent.ACTION_POWER_CONNECTED);
+        filter.addAction(Intent.ACTION_POWER_DISCONNECTED);
+
+        this.registerReceiver(mReceiver, filter);
+    }
+
+    @Override
+    protected void onDestroy() {
+        this.unregisterReceiver(mReceiver);
+        super.onDestroy();
     }
 }
