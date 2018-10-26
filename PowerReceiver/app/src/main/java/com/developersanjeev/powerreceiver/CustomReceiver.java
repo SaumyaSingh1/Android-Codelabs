28a7bbcd33b172aae4cabd7274fd4d098b042584
Sanjeev <thedevelopersanjeev@gmail.com>
2018-10-26 20:31:20 +0530

    added starter code for Custom Receiver

---
+++ PowerReceiver/app/src/main/java/com/developersanjeev/powerreceiver/CustomReceiver.java
@@ -0,0 +1,14 @@
+package com.developersanjeev.powerreceiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class CustomReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
