38449ba85d658d9fe5196cd0ead94e2b3bd0a724
Sanjeev <thedevelopersanjeev@gmail.com>
2018-10-27 02:23:34 +0530

    completed NotifyMe codelab

--- NotifyMe/app/src/main/java/com/developersanjeev/notifyme/MainActivity.java
+++ NotifyMe/app/src/main/java/com/developersanjeev/notifyme/MainActivity.java
@@ -3,7 +3,10 @@ package com.developersanjeev.notifyme;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
@@ -21,8 +24,17 @@ public class MainActivity extends AppCompatActivity {
     private Button button_update;
     private static final String PRIMARY_CHANNEL_ID = "primary_notification_channel";
     private static final int NOTIFICATION_ID = 0;
+    private static final String ACTION_UPDATE_NOTIFICATION =
+            "com.example.android.notifyme.ACTION_UPDATE_NOTIFICATION";
+    private NotificationReceiver mReceiver;
     private NotificationManager mNotifyManager;
     private Intent notificationIntent;
+
+    @Override
+    protected void onDestroy() {
+        unregisterReceiver(mReceiver);
+        super.onDestroy();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +42,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         createNotificationChannel();
+
+        mReceiver = new NotificationReceiver();
+        registerReceiver(mReceiver, new IntentFilter(ACTION_UPDATE_NOTIFICATION));
 
         notificationIntent = new Intent(this, MainActivity.class);
 
@@ -120,9 +135,29 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void sendNotification(){
+
+        Intent updateIntent = new Intent(ACTION_UPDATE_NOTIFICATION);
+        PendingIntent updatePendingIntent = PendingIntent.getBroadcast
+                (this, NOTIFICATION_ID, updateIntent, PendingIntent.FLAG_ONE_SHOT);
+
         NotificationCompat.Builder builder = getNotificationBuilder();
+
+        builder.addAction(R.drawable.ic_action_name, "Update Notification", updatePendingIntent);
+
         mNotifyManager.notify(NOTIFICATION_ID,builder.build());
 
         setNotificationButtonState(false, true, true);
+    }
+
+    public class NotificationReceiver extends BroadcastReceiver {
+
+        public NotificationReceiver() {
+        }
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            // Update the notification
+            updateNotification();
+        }
     }
 }
