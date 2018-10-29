1c6af9e478d607b2b71f64ea6a2ad291ff2436d3
Sanjeev <thedevelopersanjeev@gmail.com>
2018-10-29 22:39:34 +0530

    task 2 completed in StandUp APp

--- StandUp/app/src/main/java/com/developersanjeev/standup/MainActivity.java
+++ StandUp/app/src/main/java/com/developersanjeev/standup/MainActivity.java
@@ -1,5 +1,14 @@
 package com.developersanjeev.standup;
 
+import android.app.AlertDialog;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.CompoundButton;
@@ -8,12 +17,17 @@ import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
 
+    private NotificationManager mNotificationManager;
+    private static final int NOTIFICATION_ID = 0;
+    private static final String PRIMARY_CHANNEL_ID = "primary_notification_channel";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         ToggleButton alarmToggle = findViewById(R.id.alarmToggle);
+        mNotificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
 
         alarmToggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
@@ -22,13 +36,48 @@ public class MainActivity extends AppCompatActivity {
                 if(isChecked){
                     // Set up the toast message in on case
                     toastMessage = getString(R.string.alarm_on_message);
+                    deliverNotification(MainActivity.this);
                 }
                 else{
                     // Set up the toast message in off case
                     toastMessage = getString(R.string.alarm_off_message);
+                    mNotificationManager.cancelAll();
                 }
                 Toast.makeText(MainActivity.this, toastMessage, Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    /**
+     * Creates a notification channel for OREO and higher
+     */
+    public void createNotificationChannel(){
+        mNotificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            NotificationChannel notificationChannel = new NotificationChannel(
+                    PRIMARY_CHANNEL_ID, getString(R.string.notification_title), NotificationManager.IMPORTANCE_HIGH);
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.RED);
+            notificationChannel.enableVibration(true);
+            notificationChannel.setDescription("Notifies every 15 minutes to stand up and walk");
+            mNotificationManager.createNotificationChannel(notificationChannel);
+        }
+    }
+
+    private void deliverNotification(Context context){
+        Intent contentIntent = new Intent(context, MainActivity.class);
+        PendingIntent contentPendingIntent = PendingIntent.getActivity
+                (context, NOTIFICATION_ID, contentIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, PRIMARY_CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_stand_up)
+                .setContentTitle(getString(R.string.stand_up_title))
+                .setContentText("You should stand up and walk around now!")
+                .setContentIntent(contentPendingIntent)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setAutoCancel(true)
+                .setDefaults(NotificationCompat.DEFAULT_ALL);
+
+        mNotificationManager.notify(NOTIFICATION_ID,builder.build() );
     }
 }
