81c931b9daeba7545a315a180299c1e990ea9956
Sanjeev <thedevelopersanjeev@gmail.com>
2018-10-29 21:43:20 +0530

    task 1 completed in StandUp App

--- StandUp/app/src/main/java/com/developersanjeev/standup/MainActivity.java
+++ StandUp/app/src/main/java/com/developersanjeev/standup/MainActivity.java
@@ -2,6 +2,9 @@ package com.developersanjeev.standup;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ToggleButton alarmToggle = findViewById(R.id.alarmToggle);
+
+        alarmToggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                String toastMessage;
+                if(isChecked){
+                    // Set up the toast message in on case
+                    toastMessage = getString(R.string.alarm_on_message);
+                }
+                else{
+                    // Set up the toast message in off case
+                    toastMessage = getString(R.string.alarm_off_message);
+                }
+                Toast.makeText(MainActivity.this, toastMessage, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
