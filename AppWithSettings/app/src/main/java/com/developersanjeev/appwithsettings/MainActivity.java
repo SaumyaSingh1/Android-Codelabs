d4d3b505cb88f2d18bd1faed2140cbb1a6476f31
Sanjeev <thedevelopersanjeev@gmail.com>
2018-11-04 21:30:29 +0530

    task 1 completed in AppSettings Codelab

--- AppWithSettings/app/src/main/java/com/developersanjeev/appwithsettings/MainActivity.java
+++ AppWithSettings/app/src/main/java/com/developersanjeev/appwithsettings/MainActivity.java
@@ -1,5 +1,7 @@
 package com.developersanjeev.appwithsettings;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +10,9 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import static com.developersanjeev.appwithsettings.SettingsActivity.KEY_PREF_EXAMPLE_SWITCH;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,6 +31,17 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        android.support.v7.preference.PreferenceManager
+                .setDefaultValues(this, R.xml.preferences, false);
+
+        SharedPreferences sharedPref =
+                android.support.v7.preference.PreferenceManager
+                        .getDefaultSharedPreferences(this);
+
+        Boolean switchPref = sharedPref.getBoolean(KEY_PREF_EXAMPLE_SWITCH, false);
+        Toast.makeText(this, switchPref.toString(),
+                Toast.LENGTH_SHORT).show();
     }
 
     @Override
@@ -44,6 +60,8 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Intent intent = new Intent(this, SettingsActivity.class);
+            startActivity(intent);
             return true;
         }
 
